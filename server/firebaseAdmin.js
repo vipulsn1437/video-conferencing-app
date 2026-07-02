d616b2ffe1879aa769e9fbe073a99fb77143edd1
vipulsn1437 <vipulsn1437@gmail.com>
2026-07-02 23:21:05 +0530

    V

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,14 +1,20 @@
-const { initializeApp, cert } = require('firebase-admin/app');
+const { initializeApp, cert, getApps } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 
-const credential = process.env.FIREBASE_PRIVATE_KEY
-  ? cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    })
-  : cert(require('./serviceAccountKey.json'));
+let credential;
 
-const app = initializeApp({ credential });
+if (process.env.FIREBASE_PRIVATE_KEY) {
+  credential = cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  });
+} else {
+  credential = cert(require('./serviceAccountKey.json'));
+}
+
+const app = getApps().length
+  ? getApps()[0]
+  : initializeApp({ credential });
 
 module.exports = { auth: getAuth(app) };
