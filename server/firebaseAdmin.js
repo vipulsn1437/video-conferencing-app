1c9288c6ff7eb4bba632b12b7730fe4db4a32c88
vipulsn1437 <vipulsn1437@gmail.com>
2026-07-02 21:27:08 +0530

    Your commit message

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,10 +1,7 @@
 const admin = require('firebase-admin');
 
-console.log('firebase-admin:', admin);
-console.log('credential:', admin.credential);
-
 admin.initializeApp({
-  credential: admin.credential.cert({
+  credential: admin.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
